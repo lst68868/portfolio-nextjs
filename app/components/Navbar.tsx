115117da9c5832a5259c3f326ed0a30b3a56edad
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-08-09 12:10:45 -0400

    navbar bug

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,7 +8,7 @@ import Themebutton from "./Themebutton";
 export default function Navbar() {
   let pathname = usePathname() || "/";
   return (
-    <Disclosure as="nav" className="sticky top-0">
+    <Disclosure as="nav">
       {({ open }) => (
         <>
           <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
