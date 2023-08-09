40a7696e2cdfb4ccae756c5c4abaa89d17ce747b
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-08-09 11:50:22 -0400

    added proper links and projects

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar() {
                 <div className="flex items-center">
                   <Link href="/">
                     <h1 className="text-2xl font-medium">
-                      Jan <span className="text-teal-500">Marshal</span>
+                      Leo <span className="text-teal-500">Tulchin</span>
                     </h1>
                   </Link>
                 </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-// import Me from "@/public/IMG-0052.png";
+import Me from "@/public/img1.jpg";
 
 export default function Home() {
   return (
@@ -14,18 +14,26 @@ export default function Home() {
         <div className="flex flex-col items-center pt-8">
           <Image
             alt="Picture of leo"
-            src={"https://placehold.co/600x400"}
+            src={Me}
             className="h-48 w-48 rounded-full object-cover object-top"
           />
           <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">
             Leo Tulchin
           </h3>
           <p className="text-gray-500 dark:text-gray-300 text-center">
-            leostulchin@gmail.com
+            Click
+            <a
+              href="https://docs.google.com/document/d/1N-iVjoFL9E3qi3YkVxwRuebd5it5VY1WUisPhMRqYhc/edit?usp=sharing"
+              target="_blank"
+            >
+              {" "}
+              <b>here</b>
+            </a>{" "}
+            to view my resume
           </p>
 
           <div className="flex space-x-5 pt-6">
-            <a href="" target="_blank">
+            <a href="https://www.github.com/lst68868" target="_blank">
               <svg
                 viewBox="0 0 1024 1024"
                 fill="currentColor"
@@ -34,7 +42,7 @@ export default function Home() {
                 <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0138.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z" />
               </svg>
             </a>
-            <a href="" target="_blank">
+            <a href="https://www.linkedin.com/in/leotulchin" target="_blank">
               <svg
                 viewBox="0 0 1024 1024"
                 fill="currentColor"
@@ -52,15 +60,6 @@ export default function Home() {
                 <path d="M928 160H96c-17.7 0-32 14.3-32 32v640c0 17.7 14.3 32 32 32h832c17.7 0 32-14.3 32-32V192c0-17.7-14.3-32-32-32zm-80.8 108.9L531.7 514.4c-7.8 6.1-18.7 6.1-26.5 0L189.6 268.9A7.2 7.2 0 01194 256h648.8a7.2 7.2 0 014.4 12.9z" />
               </svg>
             </a>
-            <a href="" target="_blank">
-              <svg
-                viewBox="0 0 1024 1024"
-                fill="currentColor"
-                className="w-8 h-8 text-teal-500 hover:text-teal-600"
-              >
-                <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm215.3 337.7c.3 4.7.3 9.6.3 14.4 0 146.8-111.8 315.9-316.1 315.9-63 0-121.4-18.3-170.6-49.8 9 1 17.6 1.4 26.8 1.4 52 0 99.8-17.6 137.9-47.4-48.8-1-89.8-33-103.8-77 17.1 2.5 32.5 2.5 50.1-2a111 111 0 01-88.9-109v-1.4c14.7 8.3 32 13.4 50.1 14.1a111.13 111.13 0 01-49.5-92.4c0-20.7 5.4-39.6 15.1-56a315.28 315.28 0 00229 116.1C492 353.1 548.4 292 616.2 292c32 0 60.8 13.4 81.1 35 25.1-4.7 49.1-14.1 70.5-26.7-8.3 25.7-25.7 47.4-48.8 61.1 22.4-2.4 44-8.6 64-17.3-15.1 22.2-34 41.9-55.7 57.6z" />
-              </svg>
-            </a>
           </div>
         </div>
 
@@ -71,18 +70,13 @@ export default function Home() {
           </p>
           <p>
             I currently work for DialectaAI, building a language-learning
-            platform with the power of AWS Lambdas and ASP.NET core. Previously,
-            I helped to build and manage Tulnoy Lumber's internal software
-            suite, including postgreSQL integration, a custom React interface,
-            and an automated, Python-powered inventory management system.
+            platform with the power of AWS Lambdas and ASP.NET core.
           </p>
           <p>
-            With my additional background as a Strategic Consultant to the
-            United States federal Government, I bring a strong background in
-            client relations and business management to my engineering
-            work.Helping organizations to implement scalable, repeatable
-            solutions to everyday problems is my ultimate combination of
-            creative expression, technical expertise, and community engagement.
+            With my additional background as a systems administrator and
+            strategic consultant to the United States federal Government, I
+            bring an expertise in client relations and business management to my
+            engineering work.
           </p>
           <p>
             I'd love to learn more about what you do and how we can help one
