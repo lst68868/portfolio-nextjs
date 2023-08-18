05f3ce479968f8fae94677597070d4216fdb682d
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-08-18 15:38:20 -0400

    updated sanity schema and frontend to match

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,6 +5,8 @@ interface Data {
   title: string;
   overview: string;
   link: string;
+  githubRepo: string;
+  backendRepo: string;
   _id: string;
   imageUrl: string;
 }
@@ -12,10 +14,12 @@ interface Data {
 async function getProjects() {
   const query = `*[_type == "project"] {
     title,
-      overview,
-      link,
-      _id,
-      "imageUrl": image.asset->url
+    overview,
+    link,
+    githubRepo,
+    backendRepo,
+    _id,
+    "imageUrl": image.asset->url
   }`;
 
   const data = await client.fetch(query);
@@ -36,7 +40,7 @@ export default async function Projects() {
         </h1>
       </div>
 
-      <div className="grid gap-y-8 sm:gap-6  sm:grid-cols-2 md:gap-6 lg:grid-cols-3 lg:gap-10 pt-8">
+      <div className="grid gap-y-8 pt-8">
         {data.map((project) => (
           <article
             key={project._id}
@@ -58,7 +62,7 @@ export default async function Projects() {
                 </h3>
               </a>
 
-              <p className=" line-clamp-3 mt-2 text-sm leading-relaxed text-gray-500 dark:text-gray-400">
+              <p className="line-clamp-3 mt-2 text-sm leading-relaxed text-gray-500 dark:text-gray-400">
                 {project.overview}
               </p>
 
@@ -68,6 +72,28 @@ export default async function Projects() {
                 className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-teal-500"
               >
                 Click here to visit!
+                <span className="block transition-all group-hover:ms-0.5">
+                  &rarr;
+                </span>
+              </a>
+
+              <a
+                href={project.githubRepo}
+                target="_blank"
+                className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-teal-500"
+              >
+                GitHub Repo
+                <span className="block transition-all group-hover:ms-0.5">
+                  &rarr;
+                </span>
+              </a>
+
+              <a
+                href={project.backendRepo}
+                target="_blank"
+                className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-teal-500"
+              >
+                Backend Repo
                 <span className="block transition-all group-hover:ms-0.5">
                   &rarr;
                 </span>
--- sanity-project/schemas/project.ts
+++ sanity-project/schemas/project.ts
@@ -1,27 +1,37 @@
 export default {
-    name: 'project',
-    title: 'Project',
-    type: 'document',
-    fields: [
-      {
-        name: 'title',
-        type: 'string',
-        title: 'Title',
-      },
-      {
-        name: 'overview',
-        type: 'string',
-        title: 'Small Overview',
-      },
-      {
-        name: 'image',
-        type: 'image',
-        title: 'Image',
-      },
-      {
-        name: 'link',
-        type: 'string',
-        title: 'Link',
-      },
-    ],
-  }
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'overview',
+      type: 'string',
+      title: 'Before You Click',
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image',
+    },
+    {
+      name: 'link',
+      type: 'string',
+      title: 'Link',
+    },
+    {
+      name: 'githubRepo',
+      type: 'string',
+      title: 'GitHub Repo',
+    },
+    {
+      name: 'backendRepo',
+      type: 'string',
+      title: 'Backend Repo',
+    },
+  ],
+}
